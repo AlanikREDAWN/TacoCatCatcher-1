59b28483b52b72d57ab2716e630d5caee0db5989
Alanik <lilia.dostie@socprep.org>
2024-07-31 17:35:42 -0400

    getting vite to finally work

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,12 +1,14 @@
 
 import { createClient } from '@supabase/supabase-js';
-// const dotenv = require('dotenv');
+import * as dotenv from 'dotenv'
 // dotenv.config();
 // Create a single supabase client for interacting with your database
+// const env = await import.meta.env;
 const supabaseUrl = 'https://mclocptznsrsjhomxkyo.supabase.co';
-const supabaseKey = import.meta.env.SUPABASE_KEY;
-console.log(eval(supabaseKey));
-const supabase = createClient(supabaseUrl, eval(supabaseKey));
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jbG9jcHR6bnNyc2pob214a3lvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAyOTg2NDQsImV4cCI6MjAzNTg3NDY0NH0.TwuqIwoGOaAx31XpxA2CVvxu5QWLeRyeyhXHxpt4-yA';
+console.log(supabaseKey);
+console.log("test");
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 
 export { supabase };
